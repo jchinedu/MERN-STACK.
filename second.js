f5fc445eb9087d21c5d712ac80b3312d24c1e8bb
jchinedu <jchinedu080@gmail.com>
2023-12-19 10:34:31 +0100

    git

--- second.js
+++ second.js
@@ -1,4 +1,4 @@
 let path = require ("path");
 let mypath ="C:/Users/USER II/Desktop/project/learning/second.js"
-let parsedpath = path.parse(mypath);
-console.log(parsedpath);
+let dirname = path.dirname(mypath);
+console.log(dirname);
